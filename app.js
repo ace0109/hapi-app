350bbb02812c43ebf6dd0f4ecd6c74fe06f81481
wangcaiyuan <caiyuan0109@gmail.com>
2020-03-23 21:36:34 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// app.js
+require('env2')('./.env')
+
+const Hapi = require('hapi')
+const config = require('./config')
+const routesHelloHapi = require('./routes/hello-hapi')
+
+const server = new Hapi.Server()
+// 配置服务器启动 host 与端口
+server.connection({
+  port: config.port,
+  host: config.host
+})
+
+const init = async () => {
+  server.route([...routesHelloHapi])
+  // 启动服务
+  await server.start()
+  console.log(`Server running at: ${server.info.uri}`)
+}
+
+init()
